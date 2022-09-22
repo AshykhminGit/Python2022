cf00ec87584ebe2738b5ec35f3adcf330753f285
AshykhminGit <65783869+AshykhminGit@users.noreply.github.com>
2022-09-22 13:26:32 +0300

    Create logic.py

---
+++ Zero_level/logic.py
@@ -0,0 +1,24 @@
+if True:  # если тру значит печатем индид тру
+    print('Indeed, true')
+
+
+if 3>2:
+    print ('Три больше двух')
+
+
+is_admin =True
+if is_admin:
+    print("its admin")
+
+#Я НАЧАЛ ПОНИМАЬБ ЛОГИКУ ВЕТВЛЕНИЯЯ!!!!! ВАДАИНА АДИН!!!
+print ("put race")
+selected_character =input()
+
+if selected_character == 'protos':  # равно равно это оператор СРАВНЕНИЕ true есть отлично нетут труе другое
+    print('Protoss is the most powerfull race')
+elif selected_character == 'zerg':
+    print('Zerg is the most weak race ')
+elif selected_character == 'terran':
+    print('Terran is the most HARD race')
+else:
+    print ("its not a race")
